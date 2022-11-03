63ec90e845b7421518f98eb3f50e800ce1f5752e
Sam Laxton <samlaxton@Sams-MacBook-Air-2.local>
2022-11-03 19:51:10 +0000

    marvel API added to javascript file

---
+++ script.js
@@ -0,0 +1,10 @@
+const marvelApi = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150"
+ 
+fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=5fea457324f6f3a564b0143d51f5e005&hash=88e821f461e9081adb5a04125eb9274a")
+    .then(function (response){
+        console.log(response);
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
