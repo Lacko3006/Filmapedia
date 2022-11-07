a643a869ad7d42acb823010eacdf610daf5fde2c
Minhazul Islam <minhazulislam@hotmail.com>
2022-11-07 19:31:00 +0000

    added some files

--- scriptApi.js
+++ scriptApi.js
@@ -1,33 +1,48 @@
+let display = document.querySelector("#display");
+let movie = document.querySelector("#movie");
+let date = document.querySelector("#date");
+let actors = document.querySelectorAll("#actors");
+let mainImg = document.querySelector("#hero");
 let searchInput = document.querySelector("#searchInput");
-
 let searchBtn = document.querySelector("#searchBtn");
 
+let btn = document.querySelector("#btn");
+
+display.style.visibility = "hidden";
 
 searchBtn.addEventListener("click", function(event){
-
     event.preventDefault();
 
-
-//http://www.omdbapi.com/?i=tt3896198&apikey=6aa91fd2
-
+btn.addEventListener("click", function(){
+alert("hello");
+})
 
 fetch("http://www.omdbapi.com/?t="+searchInput.value+"&apikey=6aa91fd2")
 .then(function(response){
+    console.log(response); 
+    if(response.status == 404){
+        alert("search not found");
+    } else if(searchInput.value === ""){
+        alert("input field cant be empty")
+    } else{
+        display.style.visibility = "visible";
+    }
     return response.json();
-})
-.then (function(data){
-    console.log(data)
-})
 
 })
+    .then (function(data){
+    console.log(data);
 
-const marvelApi = "http://gateway.marvel.com/v1/public/comics?ts=1&apikey=1234&hash=ffd275c5130566a2916217b101f26150"
- 
-fetch("http://gateway.marvel.com/v1/public/comics?ts=1&apikey=5fea457324f6f3a564b0143d51f5e005&hash=88e821f461e9081adb5a04125eb9274a")
-    .then(function (response){
-        console.log(response);
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data);
-    })
+    let actorQuan = data.Actors;
+    let one = actorQuan.split(",");
+    console.log(one.length);
+
+        for(let i=0; i<one.length; i++){
+            actors[i].textContent = one[i];
+            movie.textContent = data.Title + " -";
+            date.textContent = data.Released;
+            mainImg.textContent = data.Poster
+        }
+    }) 
+})
+
